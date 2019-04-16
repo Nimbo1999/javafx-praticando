5760c7f473289dd7c6bbfd675716319c887ff085
Matheus Lopes <matlopes1999@gmail.com>
2019-04-16 11:34:53 -0300

    Adding database access

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,217 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	private Connection conn = null;
+	
+	public DepartmentDaoJDBC(Connection conn)
+	{
+		this.conn = conn;
+	}
+
+	@Override
+	public void insert(Department obj) {
+		
+		PreparedStatement st = null;
+		
+		try
+		{
+			st = conn.prepareStatement("INSERT INTO department(Name) Values(?);", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0)
+			{
+				
+				ResultSet rs = st.getGeneratedKeys();
+				
+				if(rs.next())
+				{
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				
+				DB.closeResultSet(rs);
+			}
+			else
+			{
+				
+				throw new DbException("No rows affected!");
+				
+			}
+			
+		}catch(SQLException e)
+		{
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatment(st);
+		}
+	}
+
+	@Override
+	public void update(Department obj) {
+
+		PreparedStatement st = null;
+		
+		try
+		{
+			st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?");
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0)
+			{
+				
+				System.out.println(rowsAffected + " Rows affecteds!");
+				
+			}
+			else
+			{
+				
+				throw new DbException("No rows affected!");
+				
+			}
+			
+		}catch(SQLException e)
+		{
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatment(st);
+		}
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+
+		PreparedStatement st = null;
+		
+		try
+		{
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0)
+			{
+				
+				System.out.println(rowsAffected + " Rows affecteds!");
+				
+			}
+			else
+			{
+				
+				throw new DbException("No rows affected!");
+				
+			}
+			
+		}catch(SQLException e)
+		{
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatment(st);
+		}
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try 
+		{
+			st = conn.prepareStatement("SELECT * FROM department WHERE Id = ?;");
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			if(rs.next())
+			{
+				Department dep = new Department();
+				dep.setId(rs.getInt("Id"));
+				dep.setName(rs.getString("Name"));
+				return dep;
+			}else 
+			{
+				return null;
+			}
+		}
+		catch(SQLException e)
+		{
+			throw new DbException(e.getMessage());
+		}
+		finally
+		{
+			DB.closeResultSet(rs);
+			DB.closeStatment(st);
+		}
+	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department obj = new Department();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		return obj;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try 
+		{
+			st = conn.prepareStatement("SELECT * FROM department;");
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			
+			while(rs.next())
+			{
+				Department dep = map.get(rs.getInt("Id"));
+				
+				if(dep == null)
+				{
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("Id"), dep);
+				}
+				
+				list.add(dep);
+				
+			}
+			return list;
+		}
+		catch(SQLException e)
+		{
+			throw new DbException(e.getMessage());
+		}
+		finally
+		{
+			DB.closeResultSet(rs);
+			DB.closeStatment(st);
+		}
+	}
+}
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,13 +2,17 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable {
+public class Department implements Serializable 
+{
+
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String name;
 	
-	public Department() {}
+	public Department()
+	{
+	}
 
 	public Department(Integer id, String name) {
 		this.id = id;
@@ -61,7 +65,4 @@ public class Department implements Serializable {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
 	
-	
-	
-
 }
